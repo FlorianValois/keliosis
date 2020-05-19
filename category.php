b3dc9d63be6b90fa6b1179acd65ef6a282138b4c
Florian Valois <contact@florian-valois.com>
2020-05-19 17:09:09 +0200

    Create category.php

---
+++ category.php
@@ -0,0 +1,35 @@
+<?php
+
+if (!defined('ABSPATH')) {
+	exit;
+}
+
+get_header();
+?>
+
+<!-- Begin content -->
+<main role="main">
+  
+  <article>
+    
+  <?php while ( have_posts() ) : the_post(); ?>
+
+<!--
+    <h1><?php echo the_title(); ?></h1>
+
+    <section>
+      
+    </section>
+-->
+
+  <?php endwhile; ?>
+    
+  </article>
+  
+  <?php get_sidebar(); ?>
+
+</main>
+<!-- End content -->
+
+<?php
+get_footer();
